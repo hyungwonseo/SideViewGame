980719299596d1e582a28733751c04ec48c3b226
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-01-24 11:23:17 +0900

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,11 +13,25 @@ public class PlayerController : MonoBehaviour
     bool goJump = false;            // 점프 개시 플래그
     bool onGround = false;          // 지면에 서있는 플래그
 
+    // 애니메이션 처리
+    Animator animator; // 애니메이터
+    public string stopAnime = "PlayerStop";
+    public string moveAnime = "PlayerMove";
+    public string jumpAnime = "PlayerJump";
+    public string goalAnime = "PlayerGoal";
+    public string deadAnime = "PlayerOver";
+    string nowAnime = "";
+    string oldAnime = "";
+
     // Start is called before the first frame update
     void Start()
     {
         // Rigidbody2D 가져오기
         rbody = this.GetComponent<Rigidbody2D>();
+        // Animator 가져오기
+        animator = GetComponent<Animator>();
+        nowAnime = stopAnime;
+        oldAnime = stopAnime;
     }
 
     // Update is called once per frame
@@ -66,6 +80,30 @@ public class PlayerController : MonoBehaviour
             rbody.AddForce(jumpPw, ForceMode2D.Impulse);  // 순간적인 힘 가하기
             goJump = false; // 점프 플래그 끄기
         }
+
+        if (onGround)
+        {
+            // 지면 위
+            if (axisH == 0)
+            {
+                nowAnime = stopAnime;       // 정지
+            }
+            else
+            {
+                nowAnime = moveAnime;       // 이동
+            }
+        }
+        else
+        {
+            // 공중
+            nowAnime = jumpAnime;
+        }
+
+        if (nowAnime != oldAnime)
+        {
+            oldAnime = nowAnime;
+            animator.Play(nowAnime);        // 애니메이션 재생
+        }
     }
 
     // 점프
@@ -73,5 +111,48 @@ public class PlayerController : MonoBehaviour
     {
         goJump = true; // 점프 플래그 켜기
         Debug.Log(" 점프 버튼 눌림! ");
+    }
+
+    // 접촉 시작
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Goal")
+        {
+            Goal();        // 골
+        }
+        else if (collision.gameObject.tag == "Dead")
+        {
+            GameOver();     // 게임 오버
+        }
+    }
+    // 골
+    public void Goal()
+    {
+        animator.Play(goalAnime);
+        gameState = "gameclear";
+        GameStop(); // 게임 중지
+    }
+    // 게임 오버
+    public void GameOver()
+    {
+        animator.Play(deadAnime);
+
+        gameState = "gameover";
+        GameStop(); // 게임 중지
+        // =====================
+        // 게임 오버 연출
+        // =====================
+        // 플레이어의 충돌 판정 비활성
+        GetComponent<CapsuleCollider2D>().enabled = false;
+        // 플레이어를 위로 튀어 오르게 하는 연출
+        rbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+    }
+    // 게임 중지
+    void GameStop()
+    {
+        // Rigidbody2D 가져오기
+        Rigidbody2D rbody = GetComponent<Rigidbody2D>();
+        // 속도를 0으로 하여 강제 정지
+        rbody.velocity = new Vector2(0, 0);
     }
 }
